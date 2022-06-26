0e5e17e41004de4eb450fdce8df6d140a1cf9dde
Vadim Deryabin <vad.deryabin@gmail.com>
2022-06-25 19:59:08 -0600

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,18 @@
-from hello import add
+from hello import toyou, add, subtract
 
 
 def test_add():
     assert add(1, 2) == 3
+
+def setup_function(function):
+    print(f" Running Setup: {function.__name__}")
+    function.x = 10
+
+
+def teardown_function(function):
+    print(f" Running Teardown: {function.__name__}")
+    del function.x
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
+  
